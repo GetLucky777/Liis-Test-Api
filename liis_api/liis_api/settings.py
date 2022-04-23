262527b8a53bf729ff1cc1daac587a0b6617c28d
Даниил Бородкин <faaanmu@gmail.com>
2022-04-23 14:43:59 +0300

    PythonAnyWhere deployment

--- liis_api/liis_api/settings.py
+++ liis_api/liis_api/settings.py
@@ -26,9 +26,12 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'GetLucky777.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -121,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
